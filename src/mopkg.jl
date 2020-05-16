d547ec7d26b24c851d1279b724b421dd993913dc
mikolajos <steamkontopg@wp.pl>
2020-05-16 08:16:45 +0200

    Dodanie testów dla drugiej funkcji

--- src/mopkg.jl
+++ src/mopkg.jl
@@ -5,4 +5,5 @@ example_func2(x)=x-2
 
 export example_func
 
+
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,14 @@
 using mopkg
 using Test
 
-@testset "mopkg.jl" begin
+@testset "Funckja 1" begin
    	@test example_func(2) == 4
 	@test example_func(-2) == 4
+end
+
+
+@testset "Funckja 2" begin
+   	@test example_func(2) == 0
+	@test example_func(-3) == -5
+	@test example_func(9) == 7
 end
