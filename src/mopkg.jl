84852aacc7bda99c7b9fc835ebb81298bc2d58c9
mikolajos <36952676+mikolajos@users.noreply.github.com>
2020-05-17 22:51:34 +0200

    Zmiana nazwy

--- src/mopkg.jl
+++ src/mopkg.jl
@@ -2,11 +2,11 @@ module mopkg
 
 include("svopt.jl")
 
-function line_optimize(f,x0,x1;EPS=10^(-4.),maxit=20,method::SVOptMethod=SVSecant())
+function secants_optimize(f,x0,x1;EPS=10^(-4.),maxit=20,method::SVOptMethod=SVSecant())
 	optimizer = method
 	optimizer(f,x0,x1;eps=EPS,N=maxit)
 end	
 
-export line_optimize,SVOptMethod
+export secants_optimize,SVOptMethod
 
 end # module
